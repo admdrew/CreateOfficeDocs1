8b46120c4bc9272b3c92d9fef198c7851f944123
admdrew <admdrew@gmail.com>
2013-09-08 21:44:19 -0500

    Initial commit
    Implemented:
    - MakeWordDoc(int numPages)

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Word;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CreateOfficeDocs1 {
+    class Program {
+        //private static Random random = new Random((int)DateTime.Now.Ticks);//thanks to McAden
+
+        class Optionis {
+            // doc type
+            [Option('t', "type", Required = true, HelpText = "Document type.")]
+            public string opDocumentType { get; set; }
+
+            // number of pages
+            //[Option('
+
+            // images yes/no
+ 
+            // output file name
+
+            // help
+            [HelpOption]
+            public string GetUsage() {
+                StringBuilder strUsage = new StringBuilder();
+                strUsage.AppendLine("CreatOfficeDocs");
+                strUsage.AppendLine("helpstuff");
+            }
+        }
+
+        private static string RandomString(int size) {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++) {
+                //ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                ch = (char)random.Next('a', 'z' + 1);
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+
+        static void Main(string[] args) {
+            //MakeWordDoc(100);
+            //MakeWordDoc(1000);
+            //MakeWordDoc(10000);
+        }
+
+        static void MakeWordDoc(int numPages) {
+            // make word application
+            Application appWord = new Application();
+
+            // make word doc
+            Document docWord;
+            string fileName = numPages.ToString() + "pages.docx";
+
+            // necessary objects?
+            // EDIT - noooope. thanks, .net 4.0
+            //object objMiss = System.Reflection.Missing.Value;
+            //object endofdoc = "\\endofdoc";
+
+            //appWord.Visible = true;
+            //docWord = appWord.Documents.Add(ref objMiss, ref objMiss, ref objMiss, ref objMiss);
+            docWord = appWord.Documents.Add();
+
+            // add 1 big paragraph per page
+            for (int i = 0; i < numPages; i++) {
+                Paragraph para;
+                para = docWord.Content.Paragraphs.Add();
+                para.Range.Text = "page " + i.ToString() + " of " + numPages.ToString() + " " + RandomString(3000);
+                if (i % 10 == 0)
+                    Console.Out.WriteLine(fileName + ": wrote page " + i.ToString() + " of " + numPages.ToString());
+                para.Range.InsertParagraphAfter();
+                docWord.Words.Last.InsertBreak(WdBreakType.wdPageBreak);
+            }
+
+            //string fileName = "C:\\Dropbox\\_dev\\visualstudio\\output\\test.docx";
+            
+            Console.Out.WriteLine("Writing to \"" + fileName + "\"");
+            docWord.SaveAs(fileName);
+
+            //clean up
+            appWord.Quit();
+            Marshal.FinalReleaseComObject(appWord);
+            Marshal.FinalReleaseComObject(docWord);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
---
+++ worksite-injection.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestApp
+{
+    class Program
+    {
+        /*
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                //ceate instance of dms object
+                Com.Interwoven.WorkSite.iManage.IManDMS dms = new Com.Interwoven.WorkSite.iManage.ManDMSClass();
+                if (dms != null)
+                {
+                    //add worksite server name
+                    Com.Interwoven.WorkSite.iManage.IManSession session = dms.Sessions.Add("10.11.228.118"); //worksite server name
+                    if (session != null)
+                    {
+                        //create server session
+                        session.Login("mmaju", "!manage5"); //worksite username, password
+                        if (session.Connected)
+                        {
+                            //Accesss Matter worklist and add a new workspace into it
+                            Com.Interwoven.WorkSite.iManage.IManWorkArea workarea = session.WorkArea;
+                            if (workarea != null)
+                            {
+                                Com.Interwoven.WorkSite.iManage.IManWorkspaces wss = workarea.RecentWorkspaces;
+                                if (wss != null)
+                                {
+                                    //access preferred database
+                                    if (session.PreferredDatabase != null)
+                                    {
+                                        //Create workspace
+                                        CreateWorkspace(session.PreferredDatabase, wss);
+                                    }
+                                }
+                            }
+
+                            //create document
+
+                            //logout
+                            session.Logout();
+
+                            //close dms
+                            dms.Close();
+                        }
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " - " + e.StackTrace);
+            }
+
+        }//*/
+
+
+        /*
+        /// <summary>
+        /// Create workspace
+        /// </summary>
+        /// <param name="db"></param>
+        private static void CreateWorkspace(Com.Interwoven.WorkSite.iManage.IManDatabase db, Com.Interwoven.WorkSite.iManage.IManWorkspaces wss)
+        {
+            try
+            {
+                Com.Interwoven.WorkSite.iManage.IManDocument doc = null;
+                Com.Interwoven.WorkSite.iManage.IManDocument3 doc3 = null;
+
+                //Create Workspace
+                Com.Interwoven.WorkSite.iManage.IManWorkspace ws = db.CreateWorkspace();
+                if (ws != null)
+                {
+                    ws.Name = "Sample Workspace - " + DateTime.Now.ToString();
+                    ws.SubType = "work";
+                    ws.Description = "Sample Workspace Description - " + DateTime.Now.ToString();
+
+                    string filename = System.IO.Path.GetTempFileName();
+                    Com.Interwoven.WorkSite.iManage.IManProfileUpdateResult result = ws.UpdateAllWithResults(filename);
+
+                    //add workspace into matter worklist
+                    wss.AddWorkspace(ws);
+
+                    //Create a sub-folder inside newly created workspace
+                    Com.Interwoven.WorkSite.iManage.IManDocumentFolder docfolder = ws.DocumentFolders.AddNewDocumentFolderInheriting("New Folder", "New Folder Description");
+                    if (docfolder != null)
+                    {
+                        //Create new document
+                        doc = docfolder.Database.CreateDocument();
+                        if (doc != null)
+                        {
+                            doc.SetAttributeByID(Com.Interwoven.WorkSite.iManage.imProfileAttributeID.imProfileDescription, "TestApp");
+                            doc.SetAttributeByID(Com.Interwoven.WorkSite.iManage.imProfileAttributeID.imProfileAuthor, db.Session.UserID);
+                            doc.SetAttributeByID(Com.Interwoven.WorkSite.iManage.imProfileAttributeID.imProfileOperator, db.Session.UserID);
+                            doc.SetAttributeByID(Com.Interwoven.WorkSite.iManage.imProfileAttributeID.imProfileType, "ANSI");
+                            doc.SetAttributeByID(Com.Interwoven.WorkSite.iManage.imProfileAttributeID.imProfileClass, "DOC");
+
+                            doc3 = doc as Com.Interwoven.WorkSite.iManage.IManDocument3;
+                            if (doc3 != null)
+                            {
+                                //Checkin document into newly created folder
+                                Com.Interwoven.WorkSite.iManage.IManCheckinResult CheckInResults = doc3.CheckInExToFolderAsNewDocumentWithResults(@"e:\TestApp.txt",
+                                        Com.Interwoven.WorkSite.iManage.imCheckinOptions.imDontKeepCheckedOut,
+                                        docfolder,
+                                        Com.Interwoven.WorkSite.iManage.imHistEvent.imHistoryNew,
+                                        "TestApp.exe", "Check in as new document", System.Environment.MachineName);
+                            }
+                        }
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + e.StackTrace);
+            }
+        }//*/
+
+    }
+}
